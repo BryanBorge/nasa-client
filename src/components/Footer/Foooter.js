fc6e23aae6f39e55757de313649b692a74a906ef
Bryan Borgesano <BryanBorgesano97@gmail.com>
2021-06-07 11:29:08 -0400

    Footer component

---
+++ src/components/Footer/Foooter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import {FaGithubSquare} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+import {FaRegAddressCard} from "react-icons/fa";
+
+const Foooter = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.links}>
+        <a href="https://github.com/BryanBorge" target="_blank" >
+          <FaGithubSquare  className={styles.icon} />
+        </a>
+        <a href="https://www.linkedin.com/in/bryan-borgesano/" target="_blank" >
+          <FaLinkedin  className={styles.icon} />
+        </a>
+      </div>
+      <h4>
+        COPYRIGHT©2021
+        <span>
+          <a href="https://bryanborge.github.io/portfolio/" target="_blank">BRYAN BORGESANO</a>
+        </span>
+      </h4>
+    </div>
+  );
+};
+
+export default Foooter;
